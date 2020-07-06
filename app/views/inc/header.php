e33e72b23bd7a399812b513ba81c1a0022234b25
franjojosip <franjojosip.jukic2@gmail.com>
2020-07-06 21:42:55 +0200

    Change file path

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -6,6 +6,7 @@
    <meta charset="utf-8">
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <link rel="icon" href="<?php echo URL_ROOT; ?>/public/images/library_icon.svg">
+   <link rel="shortcut icon" href="">
    <link rel="stylesheet" href="<?php echo URL_ROOT; ?>/public/css/style.css">
    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
