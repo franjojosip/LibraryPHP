02477b8fc043c48ce69908057d882e7b7f672a91
franjojosip <franjojosip.jukic2@gmail.com>
2020-07-06 14:14:53 +0200

    Change index

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
-require_once(__DIR__."\..\app\bootstrap.php");
+require_once(__DIR__."/../bootstrap.php");
 
 // Init Core Library
 $init = new Core();
