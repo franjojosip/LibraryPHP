29d60e3e0901f4477fc7cf7456ec8519ab010d5c
franjojosip <franjojosip.jukic2@gmail.com>
2020-07-06 13:42:24 +0200

    Fix bootstrap php

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,14 +1,14 @@
 <?php
 
 // Load Config
-require_once $_SERVER['DOCUMENT_ROOT'] . 'config/config.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/config/config.php';
 
 // Load Helpers
-require_once $_SERVER['DOCUMENT_ROOT'] . 'helpers/url_helper.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . 'helpers/session_helper.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/helpers/url_helper.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/helpers/session_helper.php';
 
 
 // Load libraries
-require_once $_SERVER['DOCUMENT_ROOT'] . 'libraries/Core.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . 'libraries/Controller.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . 'libraries/Database.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/libraries/Core.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/libraries/Controller.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/libraries/Database.php';
