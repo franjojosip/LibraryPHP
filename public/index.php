f2362d6ca551b8ae9afaf04e0b30574e524522c4
franjojosip <franjojosip.jukic2@gmail.com>
2020-07-06 14:05:59 +0200

    Change composer json

--- public/index.php
+++ public/index.php
@@ -2,8 +2,7 @@
 
 error_reporting(-1);
 ini_set('display_errors', 'On');
-echo realpath(__DIR__ ."..\\..");
-require_once(realpath(__DIR__ ."..\\..")."/app/bootstrap.php");
+require_once(__DIR__."/../app/bootstrap.php");
 
 // Init Core Library
 $init = new Core();
