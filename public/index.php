7f5c503aab5d56bf96a1a8521b515bf3f3e3a068
franjojosip <franjojosip.jukic2@gmail.com>
2020-07-06 13:59:44 +0200

    Change index php

--- public/index.php
+++ public/index.php
@@ -2,8 +2,8 @@
 
 error_reporting(-1);
 ini_set('display_errors', 'On');
-echo $_SERVER['DOCUMENT_ROOT'];
-require_once($_SERVER['DOCUMENT_ROOT']  .'/app/bootstrap.php');
+echo realpath(__DIR__ ."..\\..");
+require_once(realpath(__DIR__ ."..\\..")."/app/bootstrap.php");
 
 // Init Core Library
 $init = new Core();
