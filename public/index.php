98e25163b763f5c133dd579c3a9396f7032866f5
franjojosip <franjojosip.jukic2@gmail.com>
2020-07-06 13:55:19 +0200

    Change index file

--- public/index.php
+++ public/index.php
@@ -2,10 +2,8 @@
 
 error_reporting(-1);
 ini_set('display_errors', 'On');
-
-require_once $_SERVER['DOCUMENT_ROOT'] .'/app/bootstrap.php';
+echo $_SERVER['DOCUMENT_ROOT'];
+require_once($_SERVER['DOCUMENT_ROOT']  .'/app/bootstrap.php');
 
 // Init Core Library
 $init = new Core();
-
-echo("<script>console.log('Route: " . $_SERVER['DOCUMENT_ROOT'] . "');</script>");
