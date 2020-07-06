c7a8686b74836702f2ba219eaa4e1c9c7a23d706
franjojosip <franjojosip.jukic2@gmail.com>
2020-07-06 16:12:50 +0200

    Update core php

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -10,11 +10,13 @@ class Core
         $url = $this->getUrl();
 
         // Look in controllers for first value
-        if (file_exists('../../app/controllers/' . ucwords($url[0]) . '.php')) {
+        if (ucwords($url[0]) != null ){
+            if(file_exists('../../app/controllers/' . ucwords($url[0]) . '.php') {
             // If exists, set as controller
             $this->currentController = ucwords($url[0]);
             // Unset 0 url
             unset($url[0]);
+            }
         }
 
         // Require the controller
