3ec8e8637afc05a9427b02e195cbbd207f5918ff
franjojosip <franjojosip.jukic2@gmail.com>
2020-07-06 13:39:10 +0200

    Change route in bootstrap.php

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,14 +1,14 @@
 <?php
 
 // Load Config
-require_once '../config/config.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . 'config/config.php';
 
 // Load Helpers
-require_once '../helpers/url_helper.php';
-require_once '../helpers/session_helper.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . 'helpers/url_helper.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . 'helpers/session_helper.php';
 
 
 // Load libraries
-require_once '../libraries/Core.php';
-require_once '../libraries/Controller.php';
-require_once '../libraries/Database.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . 'libraries/Core.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . 'libraries/Controller.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . 'libraries/Database.php';
